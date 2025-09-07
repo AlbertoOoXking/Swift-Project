cdffb3d38020d5da8329737a60be4d7c34df9d62
AlbertoOoXking <alberteskef3@gmail.com>
2025-09-07 17:18:12 +0200

    First Commit

---
+++ Swift-Project/Swift_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ProjectApp.swift
+//  Swift-Project
+//
+//  Created by AlbertoOoXking on 07.09.25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
